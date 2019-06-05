1a960c855c783f1c311ded49127943babcb1bb59
Junyoung McArdle <juny.mcardle@gmail.com>
2019-06-05 22:52:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,12 +72,7 @@ class TicTacToe
 
    def won?
      WIN_COMBINATIONS.detect do |win_combo|
-       @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[1]] == @board[win_combo[2]]
-       if position_taken?
-         return win_combo
-       else
-         turn
-       end
+       @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[1]] == @board[win_combo[2]] && position_taken?
      end
    end
 
